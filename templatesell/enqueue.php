9e0721bf66a999f4bb1c91040cb139ebbc97324e
ramesh khanal <ramesh.khanal@swifttech.com>
2020-06-15 19:43:15 +0545

    font changed

--- templatesell/enqueue.php
+++ templatesell/enqueue.php
@@ -7,9 +7,9 @@ function springy_scripts() {
 	/*google font  */
 	global $springy_theme_options;
     /*body  */
-    wp_enqueue_style('springy-body', '//fonts.googleapis.com/css?family=Roboto:400,400i,500,600,700&display=swap', array(), null);
+    wp_enqueue_style('springy-body', '//fonts.googleapis.com/css?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', array(), null);
     /*heading  */
-    wp_enqueue_style('springy-heading', '//fonts.googleapis.com/css?family=Roboto:500,600,700&display=swap', array(), null);
+    wp_enqueue_style('springy-heading', '//fonts.googleapis.com/css?family=Hind:wght@300;400;500;600&display=swap', array(), null);
     /*Author signature google font  */
     wp_enqueue_style('springy-sign', '//fonts.googleapis.com/css?family=Monsieur+La+Doulaise&display=swap', array(), null);
     
